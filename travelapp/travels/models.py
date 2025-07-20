99df7394768c7d30b80c1c07feaa5c4c013f48f2
VoVanMai <maivo0902@gmail.com>
2025-07-20 20:39:16 +0700

    init project

---
+++ travelapp/travels/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    pass
+
+
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
